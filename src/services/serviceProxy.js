d1088706363354579e38ebd220d2db5dcb66f7bc
梁亚辉 <liangyahui@elensdata.com>
2019-03-11 19:47:02 +0800

    update

--- src/services/serviceProxy.js
+++ src/services/serviceProxy.js
@@ -4,9 +4,10 @@ export default function proxy(actions) {
   const ret = {};
   for (let key in actions) {
     if (hasOwnProperty.call(actions, key)) {
+      const _this = actions[key];
       ret[key] = async function() {
         try {
-          return await actions[key](arguments);
+          return await actions[key].call(_this, arguments);
         } catch(e) {
           const { dispatch } = arguments[0]; 
           return dispatch('Common/error', e, {root: true});
