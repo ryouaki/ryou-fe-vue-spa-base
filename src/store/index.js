0d5712959b48b5819dfb4f125a17c2760b48290d
梁亚辉 <liangyahui@elensdata.com>
2019-03-11 20:10:07 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,15 @@ import Vuex from 'vuex';
 import Common from './modules/common';
 import User from './modules/user';
 
-import actions from '@/services';
+import controllers from '@/controllers';
 import getters from './getters';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    User: User(actions.login),
-    Common: Common(actions.common)
+    User: User(controllers.login),
+    Common: Common(controllers.common)
   },
   getters
 });
